bd771b60e3d7eeab5aca32b0aaa6f5f850408cdf
HTBR <henri@htbr.nl>
2020-05-21 15:48:56 +0200

    Matrix is now an object

--- Conway.Engine.Tests/WillBeAliveTests.cs
+++ Conway.Engine.Tests/WillBeAliveTests.cs
@@ -7,17 +7,15 @@ namespace Conway.Engine.Tests
 
     public class WillBeAliveTests
     {
-        private bool[,,] _matrix;
-
-        private const int MinimumNumberOfNeighboursToBecomeAlive = 9;
+        private Matrix _matrix;
 
         [SetUp]
         public void Setup()
         {
-            _matrix = new bool[3, 3, 3];
+            _matrix = new Matrix();
         }
 
-        private bool ReturnStatusOfCenterCell() => WillBeAlive(1, 1, 1, _matrix);
+        private bool ReturnStatusOfCenterCell() => _matrix.WillBeAlive(1, 1, 1);
 
         [Test]
         public void DeadWithoutNeighbours_StillDead()
@@ -28,25 +26,61 @@ namespace Conway.Engine.Tests
         [Test]
         public void DeadWithNineNeighbourse_Alive()
         {
-            _matrix[0, 0, 0] = true;
-            _matrix[0, 0, 1] = true;
-            _matrix[0, 0, 2] = true;
-            _matrix[1, 0, 0] = true;
-            _matrix[1, 0, 1] = true;
-            _matrix[1, 0, 2] = true;
-            _matrix[2, 0, 0] = true;
-            _matrix[2, 0, 1] = true;
-            _matrix[2, 0, 2] = true;
+            _matrix.SetCell(0, 0, 0);
+            _matrix.SetCell(0, 0, 1);
+            _matrix.SetCell(0, 0, 2);
+            _matrix.SetCell(1, 0, 0);
+            _matrix.SetCell(1, 0, 1);
+            _matrix.SetCell(1, 0, 2);
+            _matrix.SetCell(2, 0, 0);
+            _matrix.SetCell(2, 0, 1);
+            _matrix.SetCell(2, 0, 2);
 
             ReturnStatusOfCenterCell().Should().BeTrue();
         }
+    }
 
-        private bool WillBeAlive(int x, int y, int z, bool[,,] matrix)
+    public class MatrixInitializationTests
+    {
+        private Matrix _matrix;
+
+        [SetUp]
+        public void Setup()
         {
-            return NumberOfNeighbours(x, y, z, _matrix) >= MinimumNumberOfNeighboursToBecomeAlive;
+            _matrix = new Matrix();
         }
 
-        private int NumberOfNeighbours(int x, int y, int z, bool[,,] matrix)
+        [Test]
+        public void NewMatrix_IsEmpty()
+        {
+            _matrix.IsEmpty().Should().BeTrue();
+        }
+
+        [Test]
+        public void SetCell_NoLongerEmpty()
+        {
+            _matrix.SetCell(0, 0, 0);
+
+            _matrix.IsEmpty().Should().BeFalse();
+        }
+    }
+
+    public class Matrix
+    {
+        private const int MinimumNumberOfNeighboursToBecomeAlive = 9;
+        private bool[,,] _matrix;
+
+        public Matrix()
+        {
+            _matrix = new bool[3, 3, 3];
+        }
+
+        public bool WillBeAlive(int x, int y, int z)
+        {
+            return NumberOfNeighbours(x, y, z) >= MinimumNumberOfNeighboursToBecomeAlive;
+        }
+
+        private int NumberOfNeighbours(int x, int y, int z)
         {
             int numberOfNeighbours = 0;
             for (int i = x - 1; i < x + 2; i++)
@@ -55,13 +89,36 @@ namespace Conway.Engine.Tests
                 {
                     for (int k = z - 1; k < z + 2; k++)
                     {
-                        if (matrix[i, j, k])
+                        if (_matrix[i, j, k])
                             numberOfNeighbours++;
                     }
                 }
             }
 
             return numberOfNeighbours;
+        }
+
+        public bool IsEmpty()
+        {
+
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k < _matrix.GetLength(2); k++)
+                    {
+                        if (_matrix[i, j, k])
+                            return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void SetCell(int x, int y, int z)
+        {
+            _matrix[x, y, z] = true;
         }
     }
 }
