34f68876c50924d08325af5797b83fcabb7f2856
HTBR <henri@htbr.nl>
2020-06-09 22:43:56 +0200

    We have a square

--- Conway.UI/MainWindow.xaml.cs
+++ Conway.UI/MainWindow.xaml.cs
@@ -1,11 +1,32 @@
 ﻿namespace Conway.UI
 {
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
+
     public partial class MainWindow
     {
         public MainWindow()
         {
             DataContext = new MainWindowViewModel();
             InitializeComponent();
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var vm = (DataContext as MainWindowViewModel);
+
+            var rect = new Rectangle();
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.StrokeThickness = 1;
+            rect.Fill = new SolidColorBrush(Colors.Black);
+            rect.Width = 200;
+            rect.Height = 200;
+            Canvas.SetLeft(rect, 0);
+            Canvas.SetTop(rect, 0);
+
+            ConwayCanvas.Children.Add(rect);
         }
     }
 }
