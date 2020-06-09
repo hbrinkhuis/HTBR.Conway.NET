74d4bf579e3ca77d43bd82b23bb556177e0859f5
HTBR <henri@htbr.nl>
2020-06-09 22:37:59 +0200

    Revert "Nevermind, let's go the game engine way"
    This reverts commit 573083e2d8a0ca083104d4f531d9a59c06d66597.

---
+++ Conway.UI/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Conway.UI
+{
+    using Engine;
+
+    public class MainWindowViewModel
+    {
+        private Matrix _matrix;
+
+        public MainWindowViewModel()
+        {
+            _matrix = new Matrix(Rows, Columns);
+        }
+
+        public int Rows { get; } = 1000;
+
+        public int Columns { get; } = 1000;
+    }
+}
